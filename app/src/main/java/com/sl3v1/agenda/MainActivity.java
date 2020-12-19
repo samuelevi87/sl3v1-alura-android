6e6c5c984def987fd270b47f9d4a03e16ff28ec5
samuelevi87 <samuel.levi.alves@outlook.com>
2020-12-19 03:51:31 -0300

    Criação de View manual diretamente no Java - Evitar

--- app/src/main/java/com/sl3v1/agenda/MainActivity.java
+++ app/src/main/java/com/sl3v1/agenda/MainActivity.java
@@ -2,7 +2,7 @@ package com.sl3v1.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -10,6 +10,16 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Samuel Levi", Toast.LENGTH_LONG).show();
+        TextView aluno = new TextView(this);
+        aluno.setText("Samuel Levi");
+        setContentView(aluno);
     }
+    /* Este modo funciona, mas não é considerado uma boa prática, por colocar
+    muita responsabilidade na activity, e ela é uma entidade que tende a crescer
+    muito durante o desenvolvimento, portanto, quanto menos responsabilidade for
+    colocar dentro dela, é melhor. Outro motivo que desencoraja a criação de views
+    de maneira manual, é a complexidade de algumas views, o que torna a manutenção
+    do código muito mais complicada.
+     */
+
 }
