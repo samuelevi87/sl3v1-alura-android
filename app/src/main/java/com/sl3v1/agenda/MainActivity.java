2fa205fa6a493e5b293f01825137be816fdb8e00
samuelevi87 <samuel.levi.alves@outlook.com>
2020-12-19 03:39:14 -0300

    Implementando o Toast.maketext

--- app/src/main/java/com/sl3v1/agenda/MainActivity.java
+++ app/src/main/java/com/sl3v1/agenda/MainActivity.java
@@ -1,6 +1,15 @@
 package com.sl3v1.agenda;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Samuel Levi", Toast.LENGTH_LONG).show();
+    }
 }
