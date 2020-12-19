9ad0e04a33de568d1d7f23bb70535941969df70b
samuelevi87 <samuel.levi.alves@outlook.com>
2020-12-19 04:09:25 -0300

    Criação do Arquivo de layout e alteração de conteúdo estático pela paleta de atributos

--- app/src/main/java/com/sl3v1/agenda/MainActivity.java
+++ app/src/main/java/com/sl3v1/agenda/MainActivity.java
@@ -10,16 +10,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView aluno = new TextView(this);
-        aluno.setText("Samuel Levi");
-        setContentView(aluno);
+        setContentView(R.layout.activity_main);
     }
-    /* Este modo funciona, mas não é considerado uma boa prática, por colocar
-    muita responsabilidade na activity, e ela é uma entidade que tende a crescer
-    muito durante o desenvolvimento, portanto, quanto menos responsabilidade for
-    colocar dentro dela, é melhor. Outro motivo que desencoraja a criação de views
-    de maneira manual, é a complexidade de algumas views, o que torna a manutenção
-    do código muito mais complicada.
-     */
 
 }
