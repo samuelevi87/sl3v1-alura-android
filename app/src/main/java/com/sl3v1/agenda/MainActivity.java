0c7574283f2d0b085d71b35825c61ad223469ac1
samuelevi87 <samuel.levi.alves@outlook.com>
2020-12-19 05:08:53 -0300

    Implementação Flexível - ListView + Adapter

--- app/src/main/java/com/sl3v1/agenda/MainActivity.java
+++ app/src/main/java/com/sl3v1/agenda/MainActivity.java
@@ -3,6 +3,8 @@ package com.sl3v1.agenda;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -17,23 +19,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         List<String> alunos = new ArrayList<>(
-                Arrays.asList("Samuel", "Deylerson", "Bruno", "Leandro"));
-        //Obtendo os campos identificados no layout
-        TextView primeiroAluno = findViewById(R.id.textView);
-        TextView segundoAluno = findViewById(R.id.textView2);
-        TextView terceiroAluno = findViewById(R.id.textView3);
-        TextView quartoAluno = findViewById(R.id.textView4);
-        //Vinculando o texto do campo obtido pela classe R
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText(alunos.get(2));
-        quartoAluno.setText(alunos.get(3));
-    }
-    /* Este método de inclusão de dados, é proposital para indicar a inflexibilidade do
-    código, ao adicionar tudo de maneira "hard code", toda vez que houver alteração na lista
-    de objetos a serem adicionados ou excluídos, deverá ser alterado a activity. Esta não é
-    uma solução viável para este tipo de comportamento. Além disso o código fica muito extenso
-    tornando mais cansativo e aumentando a propensão à erros.
-    * */
+                Arrays.asList("Samuel", "Deylerson", "Bruno", "Leandro", "Maria", "Ana"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_listaDeAlunos);
 
+        /* Diferentemente do TextView, não existe o método setText para o ListView, o procedimento
+        * é diferente, pois não há métodos get e set. Será usado um componente intermediário que
+        * ficará responsável por coletar os dados e renderizar com a view desejada. Este componente
+        * é o Adapter*/
+
+        listaDeAlunos.setAdapter(new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos));
+
+    }
 }
